453873e75f3de020e3c051ca0a5e93aea859e2a9
YeJinii <yeagin98@naver.com>
2021-08-21 00:28:05 +0900

    메인 화면 drawer 구현

--- app/src/main/java/com/example/sighome_v00/MainActivity.java
+++ app/src/main/java/com/example/sighome_v00/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.sighome_v00;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,7 +28,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-
+//060700
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
@@ -29,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
     private FirebaseUser mUser;
     private TextView mTvUserName, mqttSendBtn;
+
+    private DrawerLayout mDrawerLayout;
+    private Context context = this;
 
     private MqttClient mqttClient;
 
@@ -44,28 +54,65 @@ public class MainActivity extends AppCompatActivity {
 
         mqttSendBtn=findViewById(R.id.mqttSend_btn);
 
-        try {
-            mqttClient = new MqttClient("tcp://192.168.0.6:1883", MqttClient.generateClientId(),null);
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-        try {
-            mqttClient.connect();
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setTitle("SIG HOME");
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.navi_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+                String title = menuItem.getTitle().toString();
+
+                if(id == R.id.inside_mode){//실내 모드
+
+                }
+                else if(id == R.id.outside_mode){//실외 모드
+
+                }
+                else if(id == R.id.app_intro){//앱 소개
+
+                }
+                else if(id == R.id.logout){//로그아웃
+
+                }
+
+                return true;
+            }
+        });
+
+//        try {
+//            mqttClient = new MqttClient("tcp://111.118.51.164:1883", MqttClient.generateClientId(),null);
+//        } catch (MqttException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            mqttClient.connect();
+//        } catch (MqttException e) {
+//            e.printStackTrace();
+//        }
 
         mqttSendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mqttSendBtn.setBackgroundResource(R.drawable.round_square);
-                try {
-                    mqttClient.publish("/phone/turnoff/enter", new MqttMessage("android".getBytes()));
-                    mqttClient.publish("/phone/turnoff/bell", new MqttMessage("android".getBytes()));
-                    mqttClient.publish("/phone/turnoff/window", new MqttMessage("android".getBytes()));
-                } catch (MqttException e) {
-                    e.printStackTrace();
-                }
+//                try {
+//                    mqttClient.publish("/phone/turnoff/enter", new MqttMessage("android".getBytes()));
+//                    mqttClient.publish("/phone/turnoff/bell", new MqttMessage("android".getBytes()));
+//                    mqttClient.publish("/phone/turnoff/window", new MqttMessage("android".getBytes()));
+//                } catch (MqttException e) {
+//                    e.printStackTrace();
+//                }
             }
         });
 
@@ -86,8 +133,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    //추가된 소스, ToolBar에 menu.xml을 인플레이트함
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 
-
+    //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case android.R.id.home:{//메뉴 버튼
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+            case R.id.logout_btn: {
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(getApplicationContext(), "로그아웃 버튼이 클릭됨", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
